d3968cc27c8fe5b6f57193603c7ae6eaafe6f06f
Ratovonirina <eric.ratovonirina@gmail.com>
2025-06-29 15:26:36 +0300

    config cors

--- config/cors.js
+++ config/cors.js
@@ -1,10 +1,31 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const allowedOrigins = [
+  "https://grainedevaleur.com",
+  /\.grainedevaleur\.com$/, 
+  "http://localhost:4200",
+  "http://localhost:4300",
+];
+
 const corsOptions = {
-  // origin: '*',
-  origin: process.env.BASE_URL_UI || "https://grainedevaleur.com",
-  methods: "*",
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true); 
+
+    const isAllowed = allowedOrigins.some((allowed) => {
+      if (allowed instanceof RegExp) {
+        return allowed.test(origin);
+      }
+      return allowed === origin;
+    });
+
+    if (isAllowed) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type,Authorization",
 };
 
