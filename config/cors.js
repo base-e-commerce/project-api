3db53a74437b710b99b889a4db4cbb1eaede07e4
Ratovonirina <eric.ratovonirina@gmail.com>
2025-07-01 20:12:54 +0300

    admin

--- config/cors.js
+++ config/cors.js
@@ -6,6 +6,8 @@ const allowedOrigins = [
   "http://46.62.146.89:4001",
   "https://grainedevaleur.com",
   "https://dev.grainedevaleur.com",
+  "https://dev.admin.grainedevaleur.com",
+  "https://admin.grainedevaleur.com",
   /^https:\/\/.*\.grainedevaleur\.com$/,
   "http://localhost:4200",
   "http://localhost:4300",
